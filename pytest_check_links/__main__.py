992060edcb60ad6b68a04cf971908686238dd66b
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-02-21 15:13:03 +0100

    Add a main entry point
    Main entry point will run pytest with our plugin loaded and enabled, and
    without python collection.

---
+++ pytest_check_links/__main__.py
@@ -0,0 +1,27 @@
+"""
+This script will run pytest with our plugin loaded and enabled,
+and with the normal python plugin disabled. This means collection
+of normal tests will be disabled, unless other plugins are set
+to collect further tests. If that is the case, you can disable
+these plugins with the py.test command line option
+"-p no:<plugin-name-here>".
+"""
+import sys
+
+
+def main(args=None):
+    import pytest
+
+    if args is None:
+        args = sys.argv[1:]
+
+    return pytest.main(args + [
+        '--check-links'
+    ], [
+        'no:python'
+        '.plugin'
+    ])
+
+
+if __name__ == '__main__':
+    sys.exit(main())
