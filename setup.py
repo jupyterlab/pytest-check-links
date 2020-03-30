9d4396bb1bcb45c5c5b159cc2050a9ae936e6522
Steven Silvester <stslve@amazon.com>
2020-03-30 04:49:41 -0500

    Add note about pbr

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup
 
+# This repository uses pbr - Python Build Reasonableness
+# The version is set by the current git tag + describe status.
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True,
