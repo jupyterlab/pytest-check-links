8cb26a7f05b6ed1cc15ac5b86ef64fbd2538308e
Steven Silvester <stslve@amazon.com>
2020-04-07 04:30:50 -0500

    Update metadata for JupyterLab

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@
 from setuptools import setup
 
 # This repository uses pbr - Python Build Reasonableness
+# https://docs.openstack.org/pbr/latest/
 # The version is set by the current git tag + describe status.
+# The rest of the config data is in setup.cfg
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True,
