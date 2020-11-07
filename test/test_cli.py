6f42a48778b77ee705ac2b3f4f04db01d2cf8cd1
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-11-07 09:47:53 -0500

    add ignore pywintypes warning

--- test/test_cli.py
+++ test/test_cli.py
@@ -8,8 +8,8 @@ def test_cli_meta():
 
 
 @pytest.mark.parametrize("example,rc,expected,unexpected", [
-    ["httpbin.md", 0, ["6 passed"], ["failed", "warning"]],
-    ["rst.rst", 1, ["2 failed", "7 passed"], ["warning"]]
+    ["httpbin.md", 0, [" 6 passed"], ["failed", "warning"]],
+    ["rst.rst", 1, [" 2 failed", " 7 passed"], ["warning"]]
 ])
 def test_cli_pass(testdir, example, rc, expected, unexpected):
     testdir.copy_example(example)
