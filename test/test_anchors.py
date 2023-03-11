cd874a36ea281a89d3cca9e7c30290208f8c42cd
Steven Silvester <steven.silvester@ieee.org>
2023-03-11 13:08:12 -0600

    Switch to pytester in tests (#103)

--- test/test_anchors.py
+++ test/test_anchors.py
@@ -1,18 +1,18 @@
-def test_anchors_local_self(testdir, anchor_args):
-    testdir.copy_example("anchors_self.html")
-    result = testdir.runpytest(*anchor_args)
+def test_anchors_local_self(pytester, anchor_args):
+    pytester.copy_example("anchors_self.html")
+    result = pytester.runpytest(*anchor_args)
     result.assert_outcomes(passed=2, failed=2)
 
 
-def test_anchors_local_other(testdir, anchor_args):
-    testdir.copy_example("anchors_self.html")
-    testdir.copy_example("anchors_other.html")
+def test_anchors_local_other(pytester, anchor_args):
+    pytester.copy_example("anchors_self.html")
+    pytester.copy_example("anchors_other.html")
     args = [*anchor_args, "anchors_other.html"]
-    result = testdir.runpytest(*args)
+    result = pytester.runpytest(*args)
     result.assert_outcomes(passed=1, failed=2)
 
 
-def test_anchors_external(testdir, anchor_args):
-    testdir.copy_example("anchors_remote.html")
-    result = testdir.runpytest(*anchor_args)
+def test_anchors_external(pytester, anchor_args):
+    pytester.copy_example("anchors_remote.html")
+    result = pytester.runpytest(*anchor_args)
     result.assert_outcomes(passed=1, failed=1)
